17538479231ac4252cc01f91a8493008db6f542a
sterrish <sterrish@hotmail.co.uk>
2015-07-18 01:59:29 +0100

    cxvxcv

---
+++ testmvc/testmvc/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testmvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public string Index()
+        {
+            return "Home";
+        }
+
+        public  ViewResult listing()
+        {
+            ViewBag.lists = new List<string>()
+{
+    "dddd",
+    "hhhh"
+};
+                        return View();
+        }
+
+    }
+}
